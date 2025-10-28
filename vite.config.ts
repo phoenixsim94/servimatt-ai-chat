b4091d1789aea81c926a0dc5ad0b92acb13b8511
phoenixsim94 <phoenixsim94@gmail.com>
2025-10-28 01:05:00 -0600

    config: test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,5 +9,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Allow access from any IP address
     port: 3000,      // You can set your desired port
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
   },
 })

276cea007ca7b1e9ee4271e0d341ef43947e9446
phoenixsim94 <phoenixsim94@gmail.com>
2025-10-28 10:43:23 -0600

    chore: config to ignore custom host and port

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0', // Allow access from any IP address
-    port: 3000,      // You can set your desired port
-  },
+  // server: {
+  //   host: '0.0.0.0', // Allow access from any IP address
+  //   port: 3000,      // You can set your desired port
+  // },
   test: {
     globals: true,
     environment: 'jsdom',
